bad933d37b18a36300c01478ec0044ab6e76f521
MhaamadAli <m.alialaawar@gmail.com>
2024-04-07 01:20:06 +0300

    created the Post model

---
+++ server/app/Models/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'caption',
+        'image_url',
+    ];
+
+    /**
+     * Define the relationship with the User model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
