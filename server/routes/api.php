f19352ead9e9688ad1ef3b15322a162ae7dfb657
MhaamadAli <m.alialaawar@gmail.com>
2024-04-07 17:46:20 +0300

    added Post routes: createPost, GetAllPosts

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -20,4 +21,9 @@ Route::controller(UserController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
+});
+
+Route::controller(PostController::class)->group(function () {
+    Route::post('create', 'create');
+    Route::get('getAll', 'getAllPosts');
 });
