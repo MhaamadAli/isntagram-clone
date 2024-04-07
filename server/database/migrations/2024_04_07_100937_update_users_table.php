3c1f86089a7a977b6b3798e526818c39407146d1
MhaamadAli <m.alialaawar@gmail.com>
2024-04-07 13:22:16 +0300

    updated users table and created posts table

--- server/database/migrations/2024_04_07_100937_update_users_table.php
+++ server/database/migrations/2024_04_07_100937_update_users_table.php
@@ -4,8 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-use function Laravel\Prompts\table;
-
 return new class extends Migration
 {
     /**
@@ -13,14 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->text('caption');
-            $table->string('image_url');
+        Schema::table('users', function (Blueprint $table) {
             $table->timestamps();
-
-            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -29,6 +21,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        //
     }
 };
