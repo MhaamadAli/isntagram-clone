ddea70660acfa0e218db5f15f7c448e6cdc4a990
MhaamadAli <m.alialaawar@gmail.com>
2024-04-10 20:29:53 +0300

    created fetchposts function

--- frontend/src/ui/pages/Home/Home.jsx
+++ frontend/src/ui/pages/Home/Home.jsx
@@ -1,11 +1,46 @@
+import React, { useState, useEffect } from "react";
+import Post from "./Post/Post";
 import SideBar from "./SideBar/SideBar";
+import userprofile from "../../../assets/post1.jpg";
+import { sendRequest } from "../../../core/tools/remote/request";
+import { requestMethods } from "../../../core/enums/requestMethods";
+import "./styles.css";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    try {
+      const res = await sendRequest(requestMethods.GET, "/getAll");
+
+      setPosts(res.data);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+  };
+
   return (
-    <>
+    <div className="flex flex-row">
       <SideBar />
-      
-    </>
+      <div className="main">
+      {
+        posts.map((post) => (
+          <Post
+            key={post.id}
+            id={post.id}
+            image_url={post.image_url}
+            caption={post.caption}
+            user={post.user}
+            created_at={post.created_at}
+          />
+        ))
+      }
+      </div>
+    </div>
   );
 };
 export default Home;
