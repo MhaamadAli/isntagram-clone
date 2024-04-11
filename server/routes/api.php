a09c9a6a7d360932af30870908628df5bc3b736f
MhaamadAli <m.alialaawar@gmail.com>
2024-04-11 18:56:27 +0300

    updated routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\post\PostController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\user\UserController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +21,7 @@ Route::controller(UserController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
+    Route::get('profile', 'profile');
 });
 
 Route::controller(PostController::class)->group(function () {
